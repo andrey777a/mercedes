d2c777c0ad007a5e85dae29dec2005e868cdb4d5
Andrey <andriiano@yandex.ru>
2022-12-11 11:37:30 +0300

    feat: add files

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 $(function(){
-    
-    $('.menu__btn').on('click', function() {
-        $('.menu__list').toggleClass('menu__list--active')
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="/AVTR/img/left.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="/AVTR/img/right.svg" alt="">',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    variableWidth: true,
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
-
-
-    $('.heroes__lider-img').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.heroes__slider-text',
-        prevArrow: '<button class="slick__btn slick-prev"><img src="/app/images/arrow-left.png" alt="prev"></button>',
-        nextArrow: '<button class="slick__btn slick-next"><img src="/app/images/arrow-rigth.png" alt="next"></button>'
-    });
-    $('.heroes__slider-text').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        asNavFor: '.heroes__lider-img',
-        arrows: false
-      });
-});
+})  
